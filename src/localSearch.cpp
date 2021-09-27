57426311f0ba6939dbff49a76908fe93bd5b25e5
maximilienmoraud <maximillien.moraud@etu.imt-lille-douai.fr>
2021-09-27 16:05:53 +0200

    maj

--- src/localSearch.cpp
+++ src/localSearch.cpp
@@ -41,7 +41,7 @@ void localSearch::stratOne(Solution & _sol) {
     srand(time(0));
     for(unsigned int i=0; i<(h)*(l); i++)
         pcesPermutation.push_back(i);
-    std::random_shuffle(std::begin(pcesPermutation),std::end(pcesPermutation), myrandomLS);
+    std::random_shuffle(begin(pcesPermutation),end(pcesPermutation), myrandomLS);
 
 
     for(unsigned int i=0; i<l*h; i++) {
